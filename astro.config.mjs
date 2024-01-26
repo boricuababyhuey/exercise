2086abbd4b49412e3806d756f2c2e200f1d60988
boricuababyhuey <157830997+boricuababyhuey@users.noreply.github.com>
2024-01-26 16:55:41 -0600

    more function work

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,10 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   // ... other configurations ...
 
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    require('@astrojs/functions'),
+  ],
 
   pages: {
     '/netlify/:anything/*': { file: '/src/functions/proxy.js', statusCode: 200 },
